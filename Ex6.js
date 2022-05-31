ffbec8a616038ccc5157aaf113eb7719120131fb
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2022-05-31 09:55:59 +0200

    Initial commit

---
+++ Ex6.js
@@ -0,0 +1,20 @@
+var arrayString = ["Alex", "Arredondo", "Esteban", "Marc"];
+var arrayA = [];
+var contador = 0;
+var contadorA = 0;
+
+for (let i = 0; i < arrayString.length; i++) {
+  for (let j = 0; j < arrayString[i].length; j++) {
+    if (arrayString[i][j] = 'a') {
+      contador++;
+      if (contador == 2) {
+        arrayA[contadorA] = arrayString[i];
+        contadorA++;
+      }
+    }
+  }
+
+  contador = 0;
+}
+document.write("Strings:" + arrayString +"<br>");
+document.write("Amb vocals A:" + arrayA);
